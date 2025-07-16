d5b08eed6c205aa1a5e02457817be5f115499860
Dams4K <dams4k@users.noreply.github.com>
2025-07-17 00:07:40 +0200

    real plugin ig

--- src/main/java/fr/dams4k/servermessager/ServerMessager.java
+++ src/main/java/fr/dams4k/servermessager/ServerMessager.java
@@ -1,11 +1,19 @@
 package fr.dams4k.servermessager;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ServerMessager {
+public class ServerMessager extends JavaPlugin {
     private static JavaPlugin plugin;
+
+    @Override
+    public void onEnable() {
+        ConsoleCommandSender sender = Bukkit.getConsoleSender();
+        sender.sendMessage("[ServerMessager] Plugin enabled");
+    }
 
     public static void init(JavaPlugin plugin) {
         ServerMessager.plugin = plugin;
